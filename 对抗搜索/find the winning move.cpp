78947df3d3a0e2145e898d0fa3bebd1740d39ef9
yik <yik_cyber@yeah.net>
2021-03-30 12:44:20 +0800

    Update find the winning move.cpp
    看似优化，实则没有优化

--- 对抗搜索/find the winning move.cpp
+++ 对抗搜索/find the winning move.cpp
@@ -5,56 +5,53 @@ char board[4][4];
 
 /*is the player winner?*/
 bool is_win(char cur){
-    int i;
+    int i, j;
+    bool dia_1 = true, dia_2 = true; 
     for(i = 0; i < 4; i++){
-        if(board[i][0] == cur && board[i][1] == cur && board[i][2] == cur && board[i][3] == cur) return true;
-        if(board[0][i] == cur && board[1][i] == cur && board[2][i] == cur && board[3][i] == cur) return true;
+        for(j = 0; j < 4; j++){
+            if(board[i][j] != cur) break;
+        }
+        if(j >= 4) return true;
+        if(board[i][i] != cur) dia_1 = false;
+        for(j = 0; j < 4; j++){
+            if(board[j][i] != cur) break;
+        }
+        if(j >= 4) return true;
+        if(board[i][3-i] != cur) dia_2 = false;
     }
-    if(board[0][0] == cur && board[1][1] == cur && board[2][2] == cur && board[3][3] == cur) return true;
-    if(board[0][3] == cur && board[1][2] == cur && board[2][1] == cur && board[3][0] == cur) return true;
+    if(dia_2 || dia_1) return true;
     return false;
 }
 
+
 int chess_num = 0;
 int inf, sup;
-int minimax_search(bool is_x){
+int minimax(bool is_x){
     if(is_win('x')) return 1;
     if(is_win('o')) return -1;
     if(chess_num == 16) return -1;
-    
-    int score;
-    if(is_x){
-        score = -100;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                if(board[i][j] != '.') continue;
-                board[i][j] = 'x';
-                chess_num++;
-                score = max(score, minimax_search(!is_x));
-                board[i][j] = '.';
-                chess_num--;
-                if(score >= sup) return score;
-                inf = max(inf, score); //更新最大下界
+
+    int score = (is_x ? -1 : 1) * 100;
+    char cur = is_x ? 'x' : 'o';
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(board[i][j] != '.') continue;
+            board[i][j] = cur;
+            chess_num++;
+            score = is_x ? max(score, minimax(!is_x)) : min(score, minimax(!is_x));
+            board[i][j] = '.';
+            chess_num--;
+            if(is_x){
+                if(score > sup) return score;
+                inf = max(inf, score);
+            }
+            else{
+                if(score < 1) return -1;
+                sup = min(sup, score);
             }
         }
-        return score;
     }
-    else{
-        score = 100;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                if(board[i][j] != '.') continue;
-                board[i][j] = 'o';
-                chess_num++;
-                score = min(score, minimax_search(!is_x));
-                board[i][j] = '.';
-                chess_num--;
-                if(score < 1) return -1; //只要有一个输了就不行
-                sup = min(sup, score); //更新最小上界
-            }
-        }
-        return score;
-    }
+    return score;
 }
 
 int main(){
@@ -71,6 +68,7 @@ int main(){
             getchar();
         }
         bool flag = false;
+        if(chess_num < 5) goto over;
         inf = -100;
         sup = 100;
         for(int i = 0; i < 4; i++){
@@ -78,7 +76,7 @@ int main(){
                 if(board[i][j] != '.') continue;
                 board[i][j] = 'x';
                 chess_num++;
-                if(minimax_search(false) == 1){
+                if(minimax(false) == 1){
                     printf("(%d,%d)\n", i, j);
                     flag = true;
                     break;
@@ -88,9 +86,11 @@ int main(){
             }
             if(flag) break;
         }
-        if(!flag) printf("#####\n");
+        over:
+            if(!flag) printf("#####\n");
     }
 }
+
 
 /*
 reference
